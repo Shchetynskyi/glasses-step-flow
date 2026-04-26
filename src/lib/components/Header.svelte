2979fa19322ca18167ba15f1cbe8fcc170f651de
Olexii Shchetinskyi <olexii@example.com>
2026-04-26 15:51:27 +0300

    add header brand

---
+++ src/lib/components/Header.svelte
@@ -0,0 +1,19 @@
+<div class="header">
+	<div class="title">Майстерня Окулярко</div>
+	
+</div>
+
+<style>
+	.header {
+		text-align: center;
+		margin-bottom: 20px;
+	}
+
+	.title {
+	font-size: 22px;
+	font-weight: 700;
+	color: #5a3b2e; /* той самий, що кнопка */
+}
+
+	
+</style>
